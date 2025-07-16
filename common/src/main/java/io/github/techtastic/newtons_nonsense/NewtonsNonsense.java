dcfa59a165ffab98a35bba5eba7d237f34acf777
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-07-16 09:09:11 -0500

    Changed Logger in main mod class

--- common/src/main/java/io/github/techtastic/newtons_nonsense/NewtonsNonsense.java
+++ common/src/main/java/io/github/techtastic/newtons_nonsense/NewtonsNonsense.java
@@ -1,16 +1,16 @@
 package io.github.techtastic.newtons_nonsense;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 import physx.PxTopLevelFunctions;
-
-import java.util.logging.Logger;
 
 public final class NewtonsNonsense {
     public static final String MOD_ID = "newtons_nonsense";
-    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public static void init() {
         // Write common init code here.
 
-        LOGGER.info("PhysX Version: " + PxTopLevelFunctions.getPHYSICS_VERSION());
+        LOGGER.info("PhysX Version: {}", PxTopLevelFunctions.getPHYSICS_VERSION());
     }
 }
